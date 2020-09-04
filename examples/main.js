bf5a0439f930f2ee1200ddb05c8d13c5275e5b6f
yeguibin <yeguibin@do1.com.cn>
2020-09-04 15:48:11 +0800

    增加组件

---
+++ examples/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+// 导入组件库
+import QwUi from './../packages'
+import './../lib/quickwork-ui.css'
+import App from './App.vue'
+
+Vue.use(QwUi)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
---
+++ packages/dialog/index.js
@@ -0,0 +1,7 @@
+import Dialog from './src/dialog.vue'
+// 为组件提供 install 安装方法，供按需引入
+Dialog.install = function (Vue) {
+  Vue.component(Dialog.name, Dialog)
+}
+// 默认导出组件
+export default Dialog
